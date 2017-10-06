8c3d1058a16ad54bebf126d04258f2677fedf095
Juan Sebastian Castelblanco <jscastelblancoh@unal.edu.co>
2017-10-05 23:23:21 -0500

    Añadir los logos de facebook y google al inicio de sesión

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001234652) do
+ActiveRecord::Schema.define(version: 20171002061633) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -37,10 +37,8 @@ ActiveRecord::Schema.define(version: 20171001234652) do
   create_table "resources", force: :cascade do |t|
     t.string "name"
     t.string "attachment"
-    t.integer "lesson_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["lesson_id"], name: "index_resources_on_lesson_id"
   end
 
   create_table "users", force: :cascade do |t|
