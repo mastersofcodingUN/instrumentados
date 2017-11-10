2441f0614c4838fdbd9ebd9385393fba06a21fea
Dago Alexander Fonseca Ramírez <daafonsecara@unal.edu.co>
2017-11-10 00:57:27 -0500

    diggest descomentado

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  #onfig.assets.digest = true
+  config.assets.digest = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
