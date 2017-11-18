81650fa2435c6f349e8de629e560b228886410fc
Unknown <iscepedaz@unal.edu.co>
2017-11-18 18:19:52 -0500

    Agregada foto de perfil(#profile,#edit). Editar perfil funciona correctamente

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
     end
   end
 
-  devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+  devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks', :registrations => "registrations"}
   #get 'resources/index'
 
   #get 'resources/new'
