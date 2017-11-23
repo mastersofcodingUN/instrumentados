7256b30cf0739298d53adaf6779ad30d645b93d7
Ivan Cepeda <iscepedaz@unal.edu.co>
2017-11-23 10:55:56 -0500

    Acoplados cambios faltantes en extras

--- app/uploaders/course_image_uploader.rb
+++ app/uploaders/course_image_uploader.rb
@@ -9,7 +9,7 @@ class CourseImageUploader < CarrierWave::Uploader::Base
     "guitarra-bateria.jpg"
   end
   
-  process resize_to_fill: [160, 180]
+  process resize_to_fill: [290, 180]
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
